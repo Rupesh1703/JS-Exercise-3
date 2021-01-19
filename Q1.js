e4422ece21f0eca193f697f3ee6af1794120bb49
rupesh1703 <54272278+rupesh1703@users.noreply.github.com>
2021-01-19 20:01:44 +0530

    Add
    data

--- Q1.js
+++ Q1.js
@@ -1,7 +1,7 @@
 let email = "suryavanshi.r12@gmail.com";
 let splitedEmail = email.split("@");
-let email1 = splitedEmail[0];
-let splitedEmail1 = email1.split("");
+let split_one = splitedEmail[0];
+let splitedEmail1 = split_one.split("");
 let splitedEmail1Length = splitedEmail1.length;
 let starredEmail = null;
 for (let i = 2; i < splitedEmail1Length - 1; i++) {
